b445792a14eb328c90bfca289aaa4bba0070912b
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-10-05 02:44:24 +0400

    Add functional hello world component

---
+++ src/component/HelloWorldFunctionalComponent.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function HelloWorldFunctionalComponent(props: any) {
+    return <h1>Hello, World!</h1>;
+}
+
+export default HelloWorldFunctionalComponent;
