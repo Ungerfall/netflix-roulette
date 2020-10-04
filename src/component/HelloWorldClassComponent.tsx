d308875405d3aa24abb5412890218905df519479
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-10-05 03:26:55 +0400

    Add class component

---
+++ src/component/HelloWorldClassComponent.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HelloWorldClassComponent extends Component {
+    render() {
+        return (
+            <h3>Hello, World!</h3>
+        );
+    }
+}
+
+export default HelloWorldClassComponent;
