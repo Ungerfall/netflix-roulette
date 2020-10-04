25158e3f5f6f00d3799b0b0f01bf028227b38c26
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-10-05 03:27:07 +0400

    Add class pure component

---
+++ src/component/HelloWorldClassPureComponent.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class HelloWorldClassPureComponent extends PureComponent {
+    render() {
+        return (
+            <h3>Hello, World!</h3>
+        );
+    }
+}
+
+export default HelloWorldClassPureComponent;
