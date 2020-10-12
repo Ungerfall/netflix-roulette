f78273ebbed033ace101117a778eeeb506812d7a
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-10-12 13:12:35 +0400

    Update webpack config
    1. add html webpack plugin
    2. add source-map devtool

--- from-scratch/webpack.config.js
+++ from-scratch/webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     context: path.join(__dirname, 'src'),
-    entry: './index.ts',
-
-    module: {
-    rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-
-    output: {
-        filename: 'bundle.ts',
-        path: path.resolve(__dirname, 'dist'),
-    },
+    entry: './index.tsx',
+    devtool: "source-map",
 
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
+
+    module: {
+      rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: '/node_modules/',
+          },
+        ],
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+      })
+    ],
 
     watch: false
 }
