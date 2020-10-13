ae0ede5fd9d86aad8881980f856835507ca874f1
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-10-13 08:46:20 +0400

    Setup babel

--- from-scratch/webpack.config.js
+++ from-scratch/webpack.config.js
@@ -2,34 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: './index.tsx',
-    devtool: "source-map",
+  context: path.join(__dirname, 'src'),
+  entry: './index.tsx',
+  devtool: "source-map",
 
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
 
-    module: {
-      rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: '/node_modules/',
-          },
-        ],
-    },
+  module: {
+    rules: [{
+      test: /\.(ts|js)x?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
 
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src', 'index.html')
-      })
-    ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    watch: false
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ],
+
+  watch: false
 }
