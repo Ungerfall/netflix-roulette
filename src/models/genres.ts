bc0aa78466144aa8b7160385d4fb3226ec1b4e59
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-11-09 10:17:37 +0400

    Add genres and movie models

---
+++ src/models/genres.ts
@@ -0,0 +1,7 @@
+export enum Genres {
+    all = "ALL",
+    documentary = "DOCUMENTARY",
+    comedy = "COMEDY",
+    horror = "HORROR",
+    crime = "CRIME"
+};
---
+++ src/models/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+    title: string;
+    releaseDate: Date;
+    genres: string[];
+    imageSrc: string | undefined;
+    id: string
+}
