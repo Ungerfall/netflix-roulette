e1c0ff11158b83b514ab4f74942b46f7fb4395ef
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-11-14 22:37:06 +0400

    Add GenresUtils to get Genres enum values

--- src/models/genres.ts
+++ src/models/genres.ts
@@ -5,3 +5,9 @@ export enum Genres {
     horror = "HORROR",
     crime = "CRIME"
 };
+
+export class GenresUtils {
+    public static GenresValues(): string[] {
+        return Object.values(Genres);
+    }
+}
