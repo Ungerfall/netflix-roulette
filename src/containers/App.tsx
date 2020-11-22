8f84573089ae26a65af683bdcca18a785b8f0c22
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-11-23 01:14:17 +0400

    Compose all new components together

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,81 +1,206 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Footer from '../components/Footer';
 import MoviesList from '../components/MoviesList';
 import Search from '../components/Search';
-import { Movie } from '../models/movie';
+import { Movie, MovieForMovieCard } from '../models/movie';
 import '../App.css';
 import GenreFilter from '../components/GenreFilter';
 import Header from '../components/Header';
+import AddMovie from '../components/AddMovie';
+import { Genres } from '../models/genres';
+import { uuidv4 } from '../utils/uuid';
+import EditMovie from '../components/EditMovie';
+import DeleteMovie from '../components/DeleteMovie';
+import { datesComparer, stringComparer } from '../utils/comparers';
 
 const imagePlaceholder: string = "http://placehold.it/325x450?text=movie-poster";
 
-const movies: Movie[] = [
+const moviesStub: Movie[] = [
     {
         title: "Pulp Fiction",
-        genres: ["Action & Adventure"],
+        genre: Genres.actionAndAdventure,
         releaseDate: new Date(2004, 12, 22),
         imageSrc: imagePlaceholder,
-        id: "e2alh2_0"
+        id: uuidv4(),
+        overview: "",
+        runtime: "",
     },
     {
         title: "Bohemian Rhapsody",
-        genres: ["Drama", "Biography", "Music"],
+        genre: Genres.drama,
         releaseDate: new Date(2003, 12, 22),
         imageSrc: imagePlaceholder,
-        id: "e2alh2_1"
+        id: uuidv4(),
+        overview: "",
+        runtime: "",
     },
     {
         title: "Kill Bill: Vol 2",
-        genres: ["Oscar winning Movie"],
+        genre: Genres.drama,
         releaseDate: new Date(1994, 12, 22),
         imageSrc: imagePlaceholder,
-        id: "e2alh2_2"
+        id: uuidv4(),
+        overview: "",
+        runtime: "",
     },
     {
         title: "Reservoir dogs",
-        genres: ["Oscar winning movie"],
+        genre: Genres.drama,
         releaseDate: new Date(1994, 12, 22),
         imageSrc: imagePlaceholder,
-        id: "e2alh2_3"
+        id: uuidv4(),
+        overview: "",
+        runtime: "",
     },
     {
         title: "Inception",
-        genres: ["Action & Adventure"],
+        genre: Genres.actionAndAdventure,
         releaseDate: new Date(2003, 12, 22),
         imageSrc: imagePlaceholder,
-        id: "e2alh2_4"
+        id: uuidv4(),
+        overview: "",
+        runtime: "",
     },
 ];
 
-const addMovie = (movie: Movie) => {
-    movies.push(movie);
+const moviesDict: Record<string, Movie> = moviesStub
+    .reduce(
+        (acc, item) => {
+            acc[item.id] = item;
+            return acc;
+        },
+        {} as Record<string, Movie>);
+
+const foundCount: number = moviesStub.length;
+
+enum AppContextType {
+    Main = 1,
+    AddMovie,
+    EditMovie,
+    DeleteMovie,
+}
+
+type AppProps = {};
+
+type AppState = {
+    contextType: AppContextType,
+    movies: Record<string, Movie>,
+    selectedMovie: Movie | undefined,
+    moviesSortComparer: (a: MovieForMovieCard, b: MovieForMovieCard) => number,
 };
 
-const foundCount: number = movies.length;
+class App extends Component<AppProps, AppState> {
+    state: AppState = {
+        contextType: AppContextType.Main,
+        movies: moviesDict,
+        selectedMovie: undefined,
+        moviesSortComparer: (a, b) => datesComparer(a.releaseDate, b.releaseDate),
+    };
 
-function App() {
-    return (
-        <>
-            <Header />
-            <main id="main">
-                <input type="button" id="btn-add-movie" value="+ ADD MOVIE"
-                    onClick={() => addMovie(
-                        {
-                            title: "New Movie",
-                            genres: ["Action & Adventure"],
-                            releaseDate: new Date(2004, 12, 22),
-                            imageSrc: imagePlaceholder,
-                            id: Date.now.toString()
-                        })} />
-                <p id="find-your-movie">FIND YOR MOVIE</p>
-                <Search />
-                <GenreFilter />
-                <p><b>{foundCount}</b> movies found</p>
-                <MoviesList movies={movies} />
-            </main>
-            <Footer />
-        </>
-    );
-}
+    closeModal = () => {
+        this.setState(prev => ({ ...prev, contextType: AppContextType.Main }))
+    };
+
+    submitAddMovie = (movie: Movie) => {
+        this.setState(prev => ({
+            ...prev,
+            contextType: AppContextType.Main,
+            movies: { ...prev.movies, [movie.id]: movie }
+        }))
+    };
+
+    saveEditMovie = (movie: Movie) => {
+        this.setState(prev => ({
+            ...prev,
+            contextType: AppContextType.Main,
+            movies: { ...prev.movies, [movie.id]: movie }
+        }));
+    };
+
+    clickMovieCardEdit = (movie: MovieForMovieCard) => {
+        this.setState(prev => ({
+            ...prev,
+            contextType: AppContextType.EditMovie,
+            selectedMovie: movie as Movie
+        }));
+    };
+
+    clickMovieCardDelete = (movie: MovieForMovieCard) => {
+        this.setState(prev => ({
+            ...prev,
+            contextType: AppContextType.DeleteMovie,
+            selectedMovie: movie as Movie
+        }));
+    };
+
+    confirmDelete = () => {
+        this.setState(prev => {
+            const key = prev.selectedMovie?.id as string;
+            const {[key]: value, ...remaning} = prev.movies;
+            return ({
+                ...prev,
+                contextType: AppContextType.Main,
+                selectedMovie: undefined,
+                movies: remaning
+            })
+        });
+    };
+
+    onSortByChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value: string = e.currentTarget.value as string;
+        if (value) {
+            this.setState(prev => {
+                let comparer: (a: MovieForMovieCard, b: MovieForMovieCard) => number;
+                if (value === "releaseDate") {
+                    comparer = (a, b) => datesComparer(a.releaseDate, b.releaseDate);
+                }
+                else if (value === "genre") {
+                    comparer = (a, b) => stringComparer(a.genre, b.genre);
+                }
+                else {
+                    comparer = (a, b) => stringComparer(a.title, b.title);
+                }
+
+                return ({
+                    ...prev,
+                    moviesSortComparer: comparer
+                });
+            });
+        }
+    };
+
+    render() {
+        return (
+            <>
+                <Header />
+                {this.state.contextType === AppContextType.AddMovie
+                    ? <AddMovie onClose={this.closeModal}
+                        onSumbit={this.submitAddMovie} />
+                    : this.state.contextType === AppContextType.EditMovie
+                    ? <EditMovie onClose={this.closeModal}
+                        onSave={this.saveEditMovie}
+                        movie={this.state.selectedMovie as Movie} />
+                    : this.state.contextType === AppContextType.DeleteMovie
+                    ? <DeleteMovie onConfirm={this.confirmDelete}
+                        onClose={this.closeModal}/>
+                    :
+                    <main id="main">
+                        <input type="button" id="btn-add-movie" value="+ ADD MOVIE"
+                            onClick={() => this.setState(prev => ({ ...prev, contextType: AppContextType.AddMovie }))} />
+                        <p id="find-your-movie">FIND YOR MOVIE</p>
+                        <Search />
+                        <GenreFilter onSortByChange={this.onSortByChange} />
+                        <p><b>{foundCount}</b> movies found</p>
+                        <MoviesList movies={this.state.movies}
+                            onMovieEdit={this.clickMovieCardEdit}
+                            onMovieDelete={this.clickMovieCardDelete}
+                            sortComparer={this.state.moviesSortComparer} />
+                    </main>
+                }
+                <Footer />
+            </>
+        );
+    }
+};
 
 export default App;
