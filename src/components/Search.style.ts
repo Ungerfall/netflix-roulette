73a7a2cf16a97ed3af877e232f4b0cb1f115cdf8
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-01-11 04:24:08 +0400

    Correct possition

--- src/components/Search.style.ts
+++ src/components/Search.style.ts
@@ -7,9 +7,7 @@ export const TextInput = styled.input.attrs(props => ({
     width: calc(100% - 380px);
     border-radius: 3px;
     opacity: 0.3;
-    position: absolute;
-    left: 100px;
-    top: 150px;
+    margin-left: 100px;
 `;
 
 export const Button = styled.input.attrs(props => ({
@@ -21,7 +19,6 @@ export const Button = styled.input.attrs(props => ({
     height: 45px;
     width: 150px;
     font-size: medium;
-    position: absolute;
     top: 150px;
     right: 100px;
 `;
